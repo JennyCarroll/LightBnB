b0948db9717e1478e731e9bd2fda980c4f8b56af
Jenny Carroll <jennygc@telus.net>
2023-03-09 20:41:13 +0000

    refactor getAllReservations to use .query

--- LightBnB_WebApp-master/server/database.js
+++ LightBnB_WebApp-master/server/database.js
@@ -85,7 +85,7 @@ const getAllReservations = function (guest_id, limit = 10) {
   // return getAllProperties(null, 2);
   return pool
     .query(
-      `SELECT reservations.id, properties.title, properties.cost_per_night, reservations.start_date, avg(property_reviews.rating) as average_rating
+      `SELECT reservations.*, properties.* id, avg(property_reviews.rating) as average_rating
   FROM reservations
   JOIN properties ON properties.id = reservations.property_id
   JOIN property_reviews ON properties.id = property_reviews.property_id
@@ -96,7 +96,7 @@ const getAllReservations = function (guest_id, limit = 10) {
       [limit]
     )
     .then((result) => {
-      // console.log(result.rows);
+      console.log(result.rows);
       return result.rows;
     })
     .catch((err) => {
